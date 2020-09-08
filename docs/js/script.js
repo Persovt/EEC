465a6b5640ca4c7cb56712d43851926783a87c03
Persovt <mza200339@gmail.com>
2020-09-08 12:17:39 +0300

    second commit

--- docs/js/script.js
+++ docs/js/script.js
@@ -3,13 +3,27 @@ const menuHome = document.querySelector(".menu__home")
 const menu =document.querySelector(".menu")
 const menuMail =document.querySelector(".menu__mail")
 const menuTeacher =document.querySelector(".menu__teacher")
+const form  = document.querySelector(".form")
+const page   = document.querySelector(".page")
+const closeBtn = document.querySelector(".close__btn")
 /*==========================-INCLUDES-========================== */
   
 /*==========================-MAIN-========================== */
 menu.addEventListener('click', () => {
-  menuHome.classList.toggle("menu__visible")
-  menuMail.classList.toggle("menu__visible")
+  
+  menuHome.classList.toggle("menu__visible");
+  menuMail.classList.toggle("menu__visible");
   menuTeacher.classList.toggle("menu__visible")
+});
+
+menuMail.addEventListener('click', () => {
+  form.classList.toggle("hide");
+  page.classList.toggle("background-white");
+});
+
+closeBtn.addEventListener('click', () => {
+  form.classList.add("hide");
+  page.classList.remove("background-white")
 })
 /*==========================-MAIN-========================== */
 
